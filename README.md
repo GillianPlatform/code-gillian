cc77a95c5a4c4de00d49f1dc8c681e42afc86316
Sacha Ayoun <sachaayoun@gmail.com>
2020-03-12 20:17:24 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSIL language for VSCode
+
+This is a very basic support for JSIL on the VSCode editor
---
+++ src/extension.ts
@@ -0,0 +1,62 @@
+import * as vscode from 'vscode';
+
+// this method is called when vs code is activated
+export function activate(context: vscode.ExtensionContext) {
+
+	let timeout: NodeJS.Timer | undefined = undefined;
+
+  // create a decorator type that we use to decorate small numbers
+  const lowerOpacityDecorationType = vscode.window.createTextEditorDecorationType({
+    opacity: '0.8'
+  });
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+    }
+    const blocks = []
+    const regExStart = /\/\*@/g;
+    const regExEnd = /\*\//g;
+		const text = activeEditor.document.getText();
+		let startMatch;
+		while (startMatch = regExStart.exec(text)) {
+      const startPos = activeEditor.document.positionAt(startMatch.index);
+      regExEnd.lastIndex = startMatch.index;
+      const endMatch = regExEnd.exec(text);
+      if(endMatch) {
+        const endPos = activeEditor.document.positionAt(endMatch.index + endMatch[0].length);
+        const decoration = { range: new vscode.Range(startPos, endPos) };
+        blocks.push(decoration);
+      }
+		}
+		activeEditor.setDecorations(lowerOpacityDecorationType, blocks);
+	}
+
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+}
